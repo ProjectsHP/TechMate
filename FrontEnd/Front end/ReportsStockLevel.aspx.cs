ca63467269196a2c04d70434c4dbdcdf2df26f6d
HLULANI PRINCE KUBAYE <217011433@student.uj.ac.za>
2022-10-05 15:14:12 +0200

    Stable before reports

--- FrontEnd/Front end/ReportsStockLevel.aspx.cs
+++ FrontEnd/Front end/ReportsStockLevel.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Front_end
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class ReportsStockLevel : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
